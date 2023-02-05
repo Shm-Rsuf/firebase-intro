f8484766de6ec9920b58dcd510c4f910bbce599f
shm_usuf <shmusuf@gmail.com>
2023-02-05 06:57:21 +0600

    firebase done

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,8 +4,10 @@ import {
   GoogleAuthProvider,
   GithubAuthProvider,
   FacebookAuthProvider,
+  signInWithEmailAndPassword,
 } from "firebase/auth";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import app from "../firebase/firebase.init";
 
 const auth = getAuth(app);
@@ -14,6 +16,9 @@ const providerGithub = new GithubAuthProvider();
 const providerFacebook = new FacebookAuthProvider();
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const navigate = useNavigate();
 
   //google login system
@@ -52,6 +57,23 @@ const Login = () => {
       });
   };
 
+  //regular login system
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        navigate("/profile");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    //reset email and password
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div className="login container mx-auto py-10">
       <h2 className="section-title text-center text-2xl text-gray-500 mb-10">
@@ -78,6 +100,45 @@ const Login = () => {
         >
           login with Facebook
         </button>
+
+        <form
+          onSubmit={handleLogin}
+          className="flex flex-col gap-3 items-start mt-5"
+        >
+          <div className="form-control flex flex-col gap-2">
+            <label htmlFor="email">Email</label>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="text"
+              id="email"
+              placeholder="Enter your email"
+              required
+              className="border py-2 px-5 rounded-md w-[20rem]"
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              id="password"
+              placeholder="Enter your password"
+              required
+              className="border py-2 px-5 rounded-md w-[20rem]"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Login"
+            className="bg-sky-500 w-full py-2 uppercase tracking-widest rounded-md text-white cursor-pointer hover:bg-sky-700 duration-300 mt-3"
+          />
+          <p>
+            Don't have an accoutn?{" "}
+            <Link to="/register" className="text-sky-500 hover:underline">
+              Register
+            </Link>
+          </p>
+        </form>
       </div>
     </div>
   );
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,110 @@
-const Register = () => {
-  return <div>Register</div>;
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import app from "../firebase/firebase.init";
+
+const auth = getAuth(app);
+
+const Register = ({ setUser, user }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    //sign up a new user
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        if (result.user) {
+          //update username
+          updateUserProfile();
+        }
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+
+    //reset inputs
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
+  const updateUserProfile = () => {
+    updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: "https://randomuser.me/api/portraits/men/40.jpg",
+    })
+      .then(() => {
+        console.log("profile updated");
+        setUser({ ...user, displayName: name });
+        navigate("/login");
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  return (
+    <div className="register flex justify-center mt-10">
+      <form
+        onSubmit={handleRegister}
+        className="flex flex-col gap-3 items-start mt-5"
+      >
+        <div className="form-control flex flex-col gap-3">
+          <label htmlFor="full-name">Full Name</label>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            id="full-name"
+            placeholder="Enter your full name"
+            required
+            className="border py-2 px-5 rounded-md w-[20rem]"
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="text"
+            id="email"
+            placeholder="Enter your email"
+            required
+            className="border py-2 px-5 rounded-md w-[20rem]"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            id="password"
+            placeholder="Enter your password"
+            required
+            className="border py-2 px-5 rounded-md w-[20rem]"
+          />
+        </div>
+        <input
+          type="submit"
+          value="Register"
+          className="bg-sky-500 w-[20rem] py-2 uppercase tracking-widest rounded-md text-white cursor-pointer hover:bg-sky-700 duration-300 mt-3"
+        />
+        <p>
+          Already have an accoutn?{" "}
+          <Link to="/login" className="text-sky-500 hover:underline">
+            Login
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
